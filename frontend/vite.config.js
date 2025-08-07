e8b0a71b083b0ba07109b2f9ef4398c223cf687d
Amar Pal <amarpal.i.a.s@gmail.com>
2025-08-07 17:28:35 +0530

    tailwind init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// frontend/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: '../frontend/dist', 
+    emptyOutDir: true,
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
-})
+});
