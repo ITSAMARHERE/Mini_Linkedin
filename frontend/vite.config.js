70e18831676c94dda66e27fbeaad0f779871db32
Amar Pal <amarpal.i.a.s@gmail.com>
2025-08-07 15:46:41 +0530

    vite init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// frontend/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: '../frontend/dist', 
+    emptyOutDir: true,
+  },
+});
