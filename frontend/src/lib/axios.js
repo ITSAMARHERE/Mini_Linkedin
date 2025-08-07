06993e8879bb6595391bd37c24b5e13f449612b7
Amar Pal <amarpal.i.a.s@gmail.com>
2025-08-07 16:47:43 +0530

    init

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "https://mini-linkedin-backend-r10r.onrender.com/api/v1"
+    : "https://mini-linkedin-backend-r10r.onrender.com/api/v1"; // same for production
+
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "https://mini-linkedin-backend-r10r.onrender.com/api/v1" : "/api/v1",
-	withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
