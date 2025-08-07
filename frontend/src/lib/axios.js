9b01c48e4ceec2a570b44b563866fd8bd2fc934f
Amar Pal <amarpal.i.a.s@gmail.com>
2025-08-08 00:32:20 +0530

    axois error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = 
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api/v1"  // Development URL
-    : "https://mini-linkedin-backend-r10r.onrender.com/api/v1/api/v1"; // Production URL (relative path since we're serving from same domain)
+    : "https://mini-linkedin-backend-r10r.onrender.com/api/v1"; // Production URL (relative path since we're serving from same domain)
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
