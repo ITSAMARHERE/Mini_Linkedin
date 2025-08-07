5b22e63b02e2fd4eaf178b21cc9a632406d58d17
Amar Pal <amarpal.i.a.s@gmail.com>
2025-08-08 00:35:45 +0530

    cors origin change

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV !== "production") {
 	app.use(
 		cors({
-			origin: "http://localhost:5173",
+			origin: "https://mini-linkedin-backend-r10r.onrender.com",
 			credentials: true,
 		})
 	);
